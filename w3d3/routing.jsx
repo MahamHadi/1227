f41815602cd09f54db5e01808efdfe7afa64fa3d
MahamHadi <148997249+MahamHadi@users.noreply.github.com>
2023-11-08 19:02:44 +0500

    done Routing

---
+++ w3d3/routing.jsx
@@ -0,0 +1,13 @@
+
+const header =()=>
+{
+    return (
+        <React.Fragment>
+            <div className="contianer"></div>
+            <div className='margin'><h1>Home</h1></div>
+      <div className='margin'><h1>About</h1></div>
+      <div className='margin'><h1>Contact</h1></div>
+        </React.Fragment>
+    )
+}
+export default header
